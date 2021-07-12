254d8fd7381010d0f57d116c678b2af30999edd4
manojreddyd <manojreddyd@manojreddy>
2021-07-12 12:47:30 +0530

    this is my first commit

---
+++ src/githubdemoproject/Test1.java
@@ -0,0 +1,17 @@
+package githubdemoproject;
+
+public class Test1 {
+	public static void main(String args[]) {
+	String hungry="yes";
+	
+	if (hungry=="yes") {
+		
+		System.out.println("eat");
+		}
+	else {
+		System.out.println("do some thing");
+	}
+
+	}
+	
+}
